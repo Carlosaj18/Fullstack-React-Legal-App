4d6780e9e23f3bb0bada4e023e3c9b052870b6f0
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-12-19 14:35:34 -0500

    Bug API

--- src/components/services/apiPaises.js
+++ src/components/services/apiPaises.js
@@ -1,27 +1,27 @@
 const URL =
-  "https://geo-battuta.net/api/country/all/?key=dd0fe950948aebe004fe7f39ce43f3c5";
+    "https://geo-battuta.net/api/country/all/?key=dd0fe950948aebe004fe7f39ce43f3c5";
 
-const fetchDatosComboBox = async () => {
-  let dataJSON;
-  let headersList = {
-    Accept: "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    "Content-Type": "application/json",
-    credentials: "same-origin",
-    crossorigin: true,
-    origin: "http://localhost:3000/",
-    "Access-Control-Allow-Origin": "*",
+  const fetchDatosComboBox = async () => {
+    let dataJSON;
+    let headersList = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      "Content-Type": "application/json",
+      credentials: "same-origin",
+      crossorigin: true,
+      origin: "http://localhost:3000/",
+      "Access-Control-Allow-Origin": "*",
+    };
+    try {
+      const response = await fetch(`${URL}`, {
+        method: "GET",
+        headers: headersList,
+      });
+      dataJSON = await response.json();
+      console.log(dataJSON);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  try {
-    const response = await fetch(`${URL}`, {
-      method: "GET",
-      headers: headersList,
-    });
-    dataJSON = await response.json();
-    console.log(dataJSON);
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-export default fetchDatosComboBox;
+  export default fetchDatosComboBox
